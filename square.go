77dbafc3e5a5c716611bdcb115b2e1d403963f4e
root <changelater@mail.com>
2019-11-16 03:29:39 +0300

    add comment for square task

--- square.go
+++ square.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	x, y int
+}
+
+type Square struct {
+	start Point
+	a     uint
+}
+
+func (s Square) End() Point {
+	/* some hardcoded stuff
+	i used the minus sign in calculation of y axis because in the attached screenshot start.y lower then end.y (???)
+	*/
+	return Point{s.start.x + int(s.a), s.start.y - int(s.a)}
+}
+
+func (s Square) Perimeter() uint {
+	const squareEdgesCount = 4
+	return s.a * squareEdgesCount
+}
+
+func (s Square) Area() uint {
+	return uint(math.Pow(float64(s.a), float64(2)))
+}
+
+func main() {
+	s := Square{Point{1, 1}, 5}
+	fmt.Println(s.End())
+	fmt.Println(s.Perimeter())
+	fmt.Println(s.Area())
+}
