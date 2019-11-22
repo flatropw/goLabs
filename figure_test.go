76a8a8d64a44aa305c7e9e365db3374dcf5f532b
root <changelater@mail.com>
2019-11-22 20:19:58 +0300

    add second task

---
+++ figure_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCircle_Area(t *testing.T) {
+	given := float64(3)
+	var c Figure = Circle{given}
+	expected := 28.274333882308138
+
+	t.Run("", func(t *testing.T) {
+		got := c.Area()
+		if got != expected {
+			t.Errorf("got: %v expected: %v given radius: %v", got, expected, given)
+		}
+	})
+}
+
+func TestCircle_Perimeter(t *testing.T) {
+	given := float64(3)
+	var c Figure = Circle{given}
+	expected := 18.84955592153876
+
+	t.Run("", func(t *testing.T) {
+		got := c.Perimeter()
+		if got != expected {
+			t.Errorf("got: %v expected: %v given radius: %v", got, expected, given)
+		}
+	})
+}
+
+func TestSquare_Area(t *testing.T) {
+	given := float64(3)
+	var s Figure = Square{given}
+	expected := float64(9)
+
+	t.Run("", func(t *testing.T) {
+		got := s.Area()
+		if got != expected {
+			t.Errorf("got: %v expected: %v given radius: %v", got, expected, given)
+		}
+	})
+}
+
+func TestSquare_Perimeter(t *testing.T) {
+	given := float64(3)
+	var s Figure = Square{given}
+	expected := float64(12)
+
+	t.Run("", func(t *testing.T) {
+		got := s.Perimeter()
+		if got != expected {
+			t.Errorf("got: %v expected: %v given radius: %v", got, expected, given)
+		}
+	})
+}
--- main.go
+++ main.go
@@ -1,1 +1,47 @@
-package goLabs
+package main
+
+import (
+	"fmt"
+	"sort"
+	"time"
+)
+
+type Person struct {
+	firstName string
+	lastName  string
+	birthDay  time.Time
+}
+
+type People []Person
+
+func (p People) Len() int {
+	return len(p)
+}
+
+func (p People) Less(i, j int) bool {
+	if p[i].birthDay == p[j].birthDay {
+		return p[i].firstName < p[j].firstName
+	}
+	return p[i].birthDay.Before(p[j].birthDay)
+}
+
+func (p People) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func (p People) String() (result string) {
+	for _, v := range p {
+		result += fmt.Sprintf("%v | %s %s\n", v.birthDay, v.firstName, v.lastName)
+	}
+
+	result += "\n"
+	return
+}
+
+func (p People) Sort() {
+	sort.Sort(p)
+}
+
+func main() {
+
+}
