42533da1fbc5217a1b40410ce830add253dbf052
root <changelater@mail.com>
2019-12-03 14:55:25 +0300

    add negative test and testcases

--- strtoint_test.go
+++ strtoint_test.go
@@ -6,13 +6,21 @@ import (
 )
 
 var tests = []struct {
-	given      string
-	expected   int
+	given    string
+	expected int
 }{
 	{"-346", -346},
 	{"250", 250},
 	{"1080", 1080},
+}
 
+var badTests = []struct {
+	given    string
+	expected int
+}{
+	{"abcd", 0},
+	{"x0b", 0},
+	{"$%#%ß∂∂ß˚∂µ8328™ª•™", 0},
 }
 
 func TestMyStrToInt(t *testing.T) {
@@ -27,6 +35,22 @@ func TestMyStrToInt2(t *testing.T) {
 	for _, test := range tests {
 		result, err := MyStrToInt2(test.given)
 		assert.Nil(t, err)
+		assert.Equal(t, test.expected, result)
+	}
+}
+
+func TestMyStrToIntBadData(t *testing.T) {
+	for _, test := range badTests {
+		result, err := MyStrToInt(test.given)
+		assert.Error(t, err)
+		assert.Equal(t, test.expected, result)
+	}
+}
+
+func TestMyStrToInt2BadData(t *testing.T) {
+	for _, test := range badTests {
+		result, err := MyStrToInt2(test.given)
+		assert.Error(t, err)
 		assert.Equal(t, test.expected, result)
 	}
 }
