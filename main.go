ffde1161d82b3a82fc6d66df90fc6fdeecdef074
root <changelater@mail.com>
2019-12-05 03:51:20 +0300

    implement simple web server
    server can handle 404 error

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		ErrorHandler(w, r, http.StatusNotFound)
+		return
+	}
+
+	_, _ = fmt.Fprint(w, "main page")
+}
+
+func ErrorHandler(w http.ResponseWriter, r *http.Request, statusCode int) {
+	w.WriteHeader(statusCode)
+	_, _ = fmt.Fprintf(w, "%d", statusCode)
+}
+
+func main() {
+	http.HandleFunc("/", mainHandler)
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
